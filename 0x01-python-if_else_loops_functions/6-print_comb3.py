1674e433583612ff41f1726c46975864a8fa382b
Semir Muhammed <semirmustafa22@gmail.com>
2023-06-07 04:42:30 +0300

    Final Update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for i in range(8):
     for j in range(1, 10):
         if (i + j) < 10:
-            print("{:d}{:d}".format(i, j), end=', ')
-print("{:d}{:d}".format(i + 1, j))
+            print("{}{}".format(i, i + j), end=', ')
+print("{}{}".format(i + 1, j))
