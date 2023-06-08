561d6c7e6176a76f0c9e4159e47dd174c1e9c520
Semir Muhammed <semirmustafa22@gmail.com>
2023-06-08 17:34:51 +0300

    Mandatory Task Update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-import add_0
 
-print("1 + 2 = {:d}".format(add(1, 2)))
+if __name__ == "__main__":
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
