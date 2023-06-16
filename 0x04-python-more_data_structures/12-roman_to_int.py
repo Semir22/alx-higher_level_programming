9d59218a02ed4e8e0a898b5924d4dfbd21f7eccf
Semir Muhammed <semirmustafa22@gmail.com>
2023-06-16 05:21:32 +0300

    Error fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,8 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return (0)
-    roman_num = {"I": 1,"V": 5,"X": 10,"L": 50,"C": 100,"D": 500,"M": 1000}
+    roman_num = {"I": 1, "V": 5, "X": 10, "L": 50,
+                  "C": 100, "D": 500, "M": 1000}
     list = [roman_num[x] for x in roman_string]
     num = 0
     for i in range(len(list)):
